9ac234f3a012567ba565a6e861c7d82cad268492
ppmakeitcount <ppmakeitcount@ppmakeitcounts-MacBook-Pro.local>
2020-04-26 13:11:01 +0530

    added my current simple template | will keep updating as i keep imporoving ;)

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h> 
+#define pb push_back
+#define fo(i, n) for(int i=0; i < n; i++)
+#define all(x) x.begin(), x.end()
+
+// ------ For debugging
+#define pline cout << "\n"
+#define deb(x) cout << x << " "
+#define debX(x) cout << #x << " " << x << "\n"
+#define debV(v) for(auto e: v) {deb(e);}; pline
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+void solve() {
+    
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ; cout.tie(NULL) ;
+
+    int t;
+    cin >> t;
+    while( t-- ) {
+        solve();
+    }
+    return 0 ;
+}
