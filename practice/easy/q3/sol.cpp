74758c99f362c7b94946adb13c9d379aecefb7bf
ppmakeitcount <ppmakeitcount@ppmakeitcounts-MacBook-Pro.local>
2020-04-27 19:52:04 +0530

    sol: CIELAB | Ciel and A-B Problem

---
+++ practice/easy/q3/sol.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+#define pb push_back
+#define fo(i, n) for(int i=0; i < n; i++)
+#define all(x) x.begin(), x.end()
+
+// ------ For debugging
+#define pline cout << "\n"
+#define deb(x) cout << x << " "
+#define debX(x) cout << #x << " " << x << "\n"
+#define debV(v) for(auto e: v) {deb(e);}; pline
+#define fa(x) cout << x << "\n" // final Ans 
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+void solve() {
+    
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ; cout.tie(NULL) ;
+
+    int A, B, x;
+    cin >> A >> B;
+    x = A - B;
+    if(x % 10 == 9) {
+        x--;
+    } else {
+        x++;
+    }
+    fa(x);
+    return 0 ;
+}
