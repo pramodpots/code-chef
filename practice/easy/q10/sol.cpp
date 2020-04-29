7538fd4d180a01e44a133566d40179f2d2d9b3bb
ppmakeitcount <pramod.potdar1@gmail.com>
2020-04-29 18:55:50 +0530

    sol: CNOTE | Chef and Notebooks

---
+++ practice/easy/q10/sol.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h> 
+#define pb push_back
+#define mp make_pair
+#define fo(i, n) for(int i=0; i < n; i++)
+#define all(x) x.begin(), x.end()
+#define si(x) scanInt()
+
+// ------ For debugging
+#define pline cout << "\n"
+#define deb(x) cout << x << " "
+#define debX(x) cout << #x << " " << x << "\n"
+#define debV(v) for(auto e: v) {deb(e);}; pline
+#define fa(x) cout << x << "\n" // final Ans 
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+void setupConfig();
+
+void solve() {
+    int x, y, k, n, p, c, d;
+    cin >> x >> y >> k >> n;
+    d = x - y;
+    bool f = false; 
+    fo(i, n) {
+        cin >> p >> c;
+        if (p >= d && c <= k)
+            f = true;
+    }
+    f ? fa("LuckyChef") : fa("UnluckyChef");
+}
+
+int main() {
+    setupConfig();
+
+    int t;
+    cin >> t;
+    while( t-- ) {
+        solve();
+    }
+    return 0 ;
+}
+
+// ---- basic setup ---
+void setupConfig() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ; cout.tie(NULL) ;
+}
