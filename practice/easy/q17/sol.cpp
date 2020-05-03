6ca37a07348fd64aa7be281765500c0d483d33f0
ppmakeitcount <pramod.potdar1@gmail.com>
2020-05-03 19:19:08 +0530

    sol: LEPERMUT | Little Elephant and Permutations

---
+++ practice/easy/q17/sol.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h> 
+#define pb push_back
+#define mp make_pair
+#define fo(i, n) for(int i=0; i < n; i++)
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define scanArr(a, n) fo(i, n) { cin >> a[i]; }
+
+// ------ For debugging
+#define pline cout << "\n"
+#define deb(x) cout << x << " "
+#define debX(x) cout << #x << " " << x << "\n"
+#define debV(v) for(auto e: v) {deb(e);}; pline
+#define fa(x) cout << x << "\n" // final Ans 
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+//---- function definations
+void setupConfig();
+
+void solve() {
+    int n, x=0, y=0;
+    cin >> n;
+    int a[n];
+    fo(i, n) cin >> a[i];
+
+    
+    for(int i = 0; i < n - 1; i++) {
+        if(a[i] > a[i+1]) x++;
+        for(int j = i + 1; j < n; j++) {
+            if(a[i] > a[j]) y++;
+        }
+    }
+    x == y ? fa("YES") : fa("NO");
+}
+
+int main() {
+    setupConfig();
+
+    int T;
+    cin >> T;
+    while( T-- ) {
+        solve();
+    }
+    return 0 ;
+}
+
+// ---- for faster io and writing to files ---
+void setupConfig() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ; cout.tie(NULL) ;
+}
