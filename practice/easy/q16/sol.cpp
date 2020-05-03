d5879863bfac1abbf102544ead4b47cfbf4ced44
ppmakeitcount <pramod.potdar1@gmail.com>
2020-05-03 18:44:44 +0530

    sol: ERROR | Chef and Feedback

---
+++ practice/easy/q16/sol.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h> 
+#define pb push_back
+#define mp make_pair
+#define fo(i, n) for(int i=0; i < n; i++)
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define scanArr(a, n) fo(i, n) { cin >> a[i]; }
+
+// ------ For debugging
+#define pline cout << "\n"
+#define deb(x) cout << x << " "
+#define debX(x) cout << #x << " " << x << "\n"
+#define debV(v) for(auto e: v) {deb(e);}; pline
+#define fa(x) cout << x << "\n" // final Ans 
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+//---- function definations
+void setupConfig();
+
+string x = "101", y = "010";
+void solve() {
+    string s;
+    cin >> s;
+    if(s.find(x) != string::npos || s.find(y) != string::npos) {
+        fa("Good");
+    } else {
+        fa("Bad");
+    }
+}
+
+int main() {
+    setupConfig();
+
+    int T;
+    cin >> T;
+    while( T-- ) {
+        solve();
+    }
+    return 0 ;
+}
+
+// ---- for faster io and writing to files ---
+void setupConfig() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ; cout.tie(NULL) ;
+}
