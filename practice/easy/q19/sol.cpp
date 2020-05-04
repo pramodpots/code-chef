873b805485feb54b5243da04293cd8a79663c5af
ppmakeitcount <pramod.potdar1@gmail.com>
2020-05-04 20:17:13 +0530

    sol: DIVIDING | Dividing Stamps

---
+++ practice/easy/q19/sol.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h> 
+#define pb push_back
+#define mp make_pair
+#define fo(i, n) for(int i=0; i < n; i++)
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define scanArr(a, n) fo(i, n) { cin >> a[i]; }
+
+// ------ For debugging
+#define pline cout << "\n"
+#define deb(x) cout << x << " "
+#define debX(x) cout << #x << " " << x << "\n"
+#define debV(v) for(auto e: v) {deb(e);}; pline
+#define fa(x) cout << x << "\n" // final Ans 
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+//---- function definations
+void setupConfig();
+
+void solve() {
+    ll n, c;
+    cin >> n;
+    vi v(n);
+    fo(i, n) cin >> v[i];
+    n = (int)(n * (n + 1)) / 2;
+    auto a = accumulate(cbegin(v), cend(v), 0);
+    n == a ? fa("YES") : fa("NO");
+}
+
+int main() {
+    setupConfig();
+
+    solve();
+    return 0;
+}
+
+// ---- for faster io and writing to files ---
+void setupConfig() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin) ;
+    freopen("output.txt", "w", stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ; cout.tie(NULL) ;
+}
